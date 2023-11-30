0709a5b7815d31a77e9598f52d307e8f663bc3e8
lukasbicus <ing.lukas.bicus@gmail.com>
2023-11-30 13:05:39 +0100

    Add hero page.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,13 @@
 import {Footer} from "@/components/Footer";
 import {Header} from "@/components/Header";
+import {Hero} from "@/components/Hero";
 import {Partners} from "@/components/Partners";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
-      Lukas
+      <Hero />
       <Partners />
       <Footer />
     </main>
---
+++ components/Hero.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import React from 'react'
+
+const data = {
+  date: '27. 11. 2023',
+  title: 'ENIESA krstí album EFARMOSA',
+  description: 'Výnimočný koncert Sandry Urbančíkovej - ENIESY',
+  image: {
+    src: '/krst.jpeg',
+    alt: 'krst'
+  }
+}
+export const Hero: React.FC = () => {
+  return (
+    <div className="bg-black  relative flex items-center justify-center overflow-hidden w-full text-white">
+      <div
+        className="relative mx-auto h-full px-4 pb-20 md:pb-10 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
+        <div className="flex flex-col items-center justify-between lg:flex-row py-16">
+          <div className="relative">
+            <div className="lg:max-w-xl lg:pr-5 relative z-40">
+              <h5 className="flex text-xl uppercase text-g1">{data.date}</h5>
+              <h2
+                className="mb-6 max-w-lg leading-snug tracking-tight text-g1 text-4xl sm:leading-snug font-bold">
+                {data.title}
+              </h2>
+              <p className="text-base text-gray-500">{data.description}</p>
+              <div className="mt-10 flex flex-col items-center md:flex-row">
+                <a href="/"
+                   className="mb-3 inline-flex h-12 w-full items-center justify-center rounded px-6 font-medium tracking-wide text-white shadow-md transition hover:bg-red-800 focus:outline-none md:mr-4 md:mb-0 md:w-auto border border-white">
+                  Vstupenky</a>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="relative hidden lg:ml-32 lg:block lg:w-1/2">
+            <div
+              className="abg-orange-400 mx-auto w-fit overflow-hidden rounded-[6rem] rounded-br-none rounded-tl-none">
+              <Image
+                src={data.image.src}
+                alt={data.image.alt}
+                width={900}
+                height={600}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden text-9xl varien absolute top-6 left-1/4 text-g/10 z-40    ">
+        About Us
+      </div>
+    </div>
+  )
+}
