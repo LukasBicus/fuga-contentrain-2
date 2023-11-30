84eeaf86340ddb869c86f74a8001eaeb798504e6
lukasbicus <ing.lukas.bicus@gmail.com>
2023-11-30 15:19:36 +0100

    Fix hover

--- components/Partners.tsx
+++ components/Partners.tsx
@@ -15,7 +15,7 @@ export const Partners: React.FC = () => {
         {data.map(item => (
           <a
             key={item.name}
-            className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25"
+            className="grid w-full min-w-[7rem] transform cursor-pointer place-items-center rounded-xl border border-blue-gray-50 bg-white px-3 py-2 transition-all hover:scale-105 hover:border-blue-gray-100 hover:bg-blue-gray-50 hover:bg-opacity-25 hover:bg-transparent"
             href={item.url}>
             <span className="my-6 grid h-24 w-24 place-items-center">
             <Image
