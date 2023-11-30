80153fef40d40b4537d8edc4b5b8ac73f5f2318c
lukasbicus <ing.lukas.bicus@gmail.com>
2023-11-30 08:50:26 +0100

    Add header

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import React from 'react'
+
+export const Header: React.FC = () => {
+  return (
+    <header className="w-full text-gray-700 bg-white shadow-sm body-font">
+      <div className="container flex flex-col p-6 mx-auto md:flex-row items-center">
+        <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
+          <Image
+            src="/fuga-logo.png"
+            alt="Fuga Logo"
+            width={175}
+            height={120}
+            priority
+          />
+        </a>
+        <nav className="flex items-center justify-center text-base md:ml-auto h-full">
+          <a href="#_" className="mr-5 font-medium hover:text-red-600">Home</a>
+          <a href="#_" className="mr-5 font-medium hover:text-red-600">About</a>
+          <a href="#_" className="font-medium hover:text-red-600">Contact</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
