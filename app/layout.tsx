4692992c5288af881b91ef25a405c946debf3766
lukasbicus <ing.lukas.bicus@gmail.com>
2023-12-04 13:45:40 +0100

    Move icon definition to the new API.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { rubikMonoOne, rubik } from './fonts'
 import type { Metadata } from 'next'
 import './globals.css'
@@ -6,6 +5,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Fuga',
   description: 'Kultúrne centrum kresťanov',
+  icons: '/favicon.ico',
 }
 
 export default function RootLayout({
@@ -15,9 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${rubik.variable} ${rubikMonoOne.variable}`}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" sizes="64x64" />
-      </Head>
       <body>{children}</body>
     </html>
   )
