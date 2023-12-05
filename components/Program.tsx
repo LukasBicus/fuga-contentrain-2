dfc8f9d5d0598cc63984a2200b346fbc31fada8a
lukasbicus <ing.lukas.bicus@gmail.com>
2023-12-05 09:55:49 +0100

    Style date in Program list.

--- components/Program.tsx
+++ components/Program.tsx
@@ -1,3 +1,10 @@
+import {
+  capitalizeFirstLetter,
+  getFormatDayNumeric,
+  getFormatTime,
+  getFormatWeekDay,
+  joinTroughDot,
+} from '@/lib/format'
 import { LocaleCode } from '@/types'
 import Image from 'next/image'
 import React from 'react'
@@ -77,52 +84,66 @@ export const Program: React.FC<{ localeCode: LocaleCode }> = async ({
     events: { items },
   } = data
 
+  const formatWeekDay = getFormatWeekDay(localeCode)
+  const formatDayNumeric = getFormatDayNumeric(localeCode)
+  const formatTime = getFormatTime(localeCode)
   return (
     <div className="w-full">
       <div className="mx-auto h-full px-4 pb-20 md:pb-10 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
         <h2 className="py-6 max-w-lg text-4xl">Program</h2>
         {/*// @ts-ignore*/}
-        {items.map((item) => (
-          <div
-            key={item.id}
-            className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5"
-          >
-            <div className="md:flex">
-              <div className="md:flex-shrink-0">
-                {item.show.primaryImage?.url ? (
-                  <Image
-                    className="h-48 w-full object-cover md:w-48"
-                    src={item.show.primaryImage.url}
-                    alt={item.show.primaryImage.key}
-                    width={135}
-                    height={202}
-                  />
-                ) : (
-                  <Image
-                    className="h-48 w-full object-cover md:w-48"
-                    src="https://attendio-library.attendio.online/5f6c59a6b32df40dd71ea8a9970cbdcb4f0294f0-poster-medium.jpeg"
-                    alt={'An alt'}
-                    width={135}
-                    height={202}
-                  />
-                )}
-              </div>
-              <div className="p-8">
-                <h5 className="uppercase tracking-wide text-sm">
-                  {item.names[localeCode]}
-                </h5>
-                <p className="mt-2 text-gray-500">{item.startsAt}</p>
-                <p className="mt-2 text-gray-500">{item.venue.name}</p>
-                <p className="block mt-1 text-lg leading-tight font-medium text-black">
-                  {item.show.translations.find(
-                    // @ts-ignore
-                    (translation) => translation.localeCode === localeCode
-                  ).tagline || item.show.translations[0].tagline}
-                </p>
+        {items.map((item) => {
+          const startsAtDate = new Date(item.startsAt)
+          return (
+            <div
+              key={item.id}
+              className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5"
+            >
+              <div className="md:flex">
+                <div className="md:flex-shrink-0">
+                  {item.show.primaryImage?.url ? (
+                    <Image
+                      className="h-48 w-full object-cover md:w-48"
+                      src={item.show.primaryImage.url}
+                      alt={item.show.primaryImage.key}
+                      width={135}
+                      height={202}
+                    />
+                  ) : (
+                    <Image
+                      className="h-48 w-full object-cover md:w-48"
+                      src="https://attendio-library.attendio.online/5f6c59a6b32df40dd71ea8a9970cbdcb4f0294f0-poster-medium.jpeg"
+                      alt={'An alt'}
+                      width={135}
+                      height={202}
+                    />
+                  )}
+                </div>
+                <div className="p-8">
+                  <h5 className="uppercase tracking-wide text-sm">
+                    {item.names[localeCode]}
+                  </h5>
+                  <p className="mt-2 text-gray-500">
+                    {capitalizeFirstLetter(
+                      joinTroughDot([
+                        formatWeekDay(startsAtDate),
+                        formatDayNumeric(startsAtDate),
+                        formatTime(startsAtDate),
+                      ])
+                    )}
+                  </p>
+                  <p className="mt-2 text-gray-500">{item.venue.name}</p>
+                  <p className="block mt-1 text-lg leading-tight font-medium text-black">
+                    {item.show.translations.find(
+                      // @ts-ignore
+                      (translation) => translation.localeCode === localeCode
+                    ).tagline || item.show.translations[0].tagline}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          )
+        })}
       </div>
     </div>
   )
---
+++ lib/format.ts
@@ -0,0 +1,30 @@
+import { LocaleCode } from '@/types'
+
+export const getFormatWeekDay = (localeCode: LocaleCode) => (date: Date) => {
+  const formatter = Intl.DateTimeFormat(localeCode, {
+    weekday: 'long',
+  })
+  return formatter.format(date)
+}
+
+export const getFormatDayNumeric = (localeCode: LocaleCode) => (date: Date) => {
+  const formatter = Intl.DateTimeFormat(localeCode, {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+  })
+  return formatter.format(date)
+}
+
+export const getFormatTime = (localeCode: LocaleCode) => (date: Date) => {
+  const formatter = Intl.DateTimeFormat(localeCode, {
+    hour: '2-digit',
+    minute: '2-digit',
+  })
+  return formatter.format(date)
+}
+
+export const joinTroughDot = (list: string[]) => list.join(' • ')
+
+export const capitalizeFirstLetter = (str: string): string =>
+  str.charAt(0).toUpperCase() + str.slice(1)
