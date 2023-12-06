02942f189627111a00f40963a0e336d991137f7d
lukasbicus <ing.lukas.bicus@gmail.com>
2023-12-06 10:55:38 +0100

    Add revalidation every 60sec

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -8,6 +8,8 @@ import { EVENT_QUERY } from '@/app/event/[id]/graphql'
 import { Header } from '@/components/Header'
 import { graphqlClient } from '@/lib/entradio-api'
 
+export const revalidate = 60 // revalidate at most every 15s
+
 export default async function Page({ params }: { params: { id: string } }) {
   const data = await graphqlClient.request<EventQuery, EventQueryVariables>(
     EVENT_QUERY,
@@ -16,7 +18,6 @@ export default async function Page({ params }: { params: { id: string } }) {
       type: ShowImageType.Poster,
     }
   )
-  console.log('d', data)
 
   const { event } = data
   return (
